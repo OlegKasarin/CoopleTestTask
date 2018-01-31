23f0c931dc5faec15b819e7495d5fe0470b24a59
Oleg Kasarin <Oleg_Kasarin@epam.com>
2018-01-31 19:41:48 +0300

    Initial Commit

---
+++ CoopleTestTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoopleTestTask
+//
+//  Created by apple on 1/31/18.
+//  Copyright © 2018 OlegKasarin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoopleTestTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoopleTestTask
+//
+//  Created by apple on 1/31/18.
+//  Copyright © 2018 OlegKasarin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
